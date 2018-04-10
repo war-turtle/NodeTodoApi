e45f8dff5cbbfbc7e3f2a780c3e35f0e4037eb48
war-turtle <kartikyadav71199@gmail.com>
2018-04-10 20:15:15 +0530

    mongoose added

---
+++ playground/mongo-db-update.js
@@ -0,0 +1,25 @@
+const {MongoClient} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017',(err, client) => {
+  
+  if(err){
+    return console.log(err);
+  }
+  console.log('connected');
+
+  const db = client.db('Todo');
+
+  const collection = db.collection('Todos');
+
+  collection.deleteOne({
+    text: 'Lunch'
+  }).then((err, result) => {
+    if(err){
+      return console.log("a",err);
+    }
+    console.log(result);
+  });
+
+  client.close();
+
+});
