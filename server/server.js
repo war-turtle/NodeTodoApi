6f2b7421bf63511d7703b53385e956266d8a9843
war-turtle <kartikyadav71199@gmail.com>
2018-04-10 21:19:29 +0530

    post and get route added

--- server/server.js
+++ server/server.js
@@ -1,41 +1,34 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todos');
+var {User} = require('./models/user');
 
-// mongoose.connection.
-var Todo = mongoose.model('Todo',{
-  text: {
-    type: String,
-    required: true,
-    minlenght: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 });
 
-var User = mongoose.model('User',{
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlenght: 1
-  }
+app.get('/todos',(req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
 });
 
-var newUser = new User({
-  email: 'kartik'
-});
-
-newUser.save().then((doc) => {
-  console.log(doc);
-}).catch((e) => {
-  console.log(e);
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
