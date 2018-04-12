bfb5bf63bbb62e1a244777a7cd2867598c38d793
war-turtle <kartikyadav71199@gmail.com>
2018-04-12 18:23:57 +0530

    POST /users and token added

--- server/server.js
+++ server/server.js
@@ -102,6 +102,21 @@ app.patch('/todos/:id',(req, res) => {
   })
 });
 
+app.post('/users',(req, res) => {
+  var body = _.pick(req.body,['email','password']);
+
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header({'x-auth': token})
+    res.send(user);
+  }).catch((e) => {
+    res.status(404).send(e);
+  })
+})
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
